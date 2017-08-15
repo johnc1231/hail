b16d7cfad84f5501e0ddd259a63e6e58e2496f05
John Compitello <johnc@broadinstitute.org>
2017-08-15 10:53:54 -0400

    Added basis for ordered join on IndexedRowMatrix

--- src/main/scala/is/hail/utils/richUtils/RichBlockMatrix.scala
+++ src/main/scala/is/hail/utils/richUtils/RichBlockMatrix.scala
@@ -4,29 +4,37 @@ import org.apache.spark.Partitioner
 import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.mllib.linalg.distributed.{BlockMatrix, IndexedRow, IndexedRowMatrix}
 import breeze.linalg.{DenseVector => BDV, Vector => BV}
+import is.hail.sparkextras.{OrderedPartitioner, OrderedRDD}
+import org.apache.spark.rdd.ShuffledRDD
 
 
 class RichBlockMatrix (bm: BlockMatrix) {
-  def toIndexedRowMatrixPartitioner(partitioner: Partitioner): IndexedRowMatrix = {
+  def toIndexedRowMatrixOrderedPartitioner(partitioner: OrderedPartitioner[Int, Int]): IndexedRowMatrix = {
     val cols = bm.numCols().toInt
 
     require(cols < Int.MaxValue, s"The number of columns should be less than Int.MaxValue ($cols).")
 
-    val rows = bm.blocks.flatMap { case ((blockRowIdx, blockColIdx), mat) =>
+    val rowsPerBlock = bm.rowsPerBlock
+    val colsPerBlock = bm.colsPerBlock
+
+    import partitioner.kOk
+    val rows = OrderedRDD(new ShuffledRDD[Int, (Int, BDV[Double]), (Int, BDV[Double])](bm.blocks.flatMap { case ((blockRowIdx, blockColIdx), mat) =>
       mat.rowIter.zipWithIndex.map {
         case (vector, rowIdx) =>
-          blockRowIdx * bm.rowsPerBlock + rowIdx -> (blockColIdx, new BDV[Double](vector.toDense.values))
+          blockRowIdx * rowsPerBlock + rowIdx -> (blockColIdx, new BDV[Double](vector.toDense.values))
       }
-    }.groupByKey(partitioner).map { case (rowIdx, vectors) =>
+    }, partitioner).setKeyOrdering(Ordering[Int]), partitioner).groupByKey().map { case (rowIdx, vectors) =>
 
       val wholeVector = BDV.zeros[Double](cols)
 
       vectors.foreach { case (blockColIdx: Int, vec: BV[Double]) =>
-        val offset = bm.colsPerBlock * blockColIdx
-        wholeVector(offset until Math.min(cols, offset + bm.colsPerBlock)) := vec
+        val offset = colsPerBlock * blockColIdx
+        wholeVector(offset until Math.min(cols, offset + colsPerBlock)) := vec
       }
       new IndexedRow(rowIdx, new DenseVector(wholeVector.data))
     }
+    println("INNER1 " + partitioner.getPartition(875))
+    println("INNER2 " + partitioner.getPartition(876))
     new IndexedRowMatrix(rows, bm.numRows(), cols)
   }
 }
