58a88c72f0765b57fa0142dc1c8db260611292e7
John Compitello <j301600@gmail.com>
2020-05-21 14:14:55 -0400

    kill the theme path

--- hail/python/hail/docs/conf.py
+++ hail/python/hail/docs/conf.py
@@ -190,7 +190,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes", ]
+#html_theme_path = ["_themes", ]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
